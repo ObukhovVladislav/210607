e18de4e1fad7b6d7a1053d6852a90f128d7cc4d7
ObukhovVladislav <vobukhov10@gmail.com>
2021-06-07 11:36:30 +0500

    index.py

--- index.py
+++ index.py
@@ -1,11 +1,12 @@
-class Student:
-    def __init__(self, name, birthday, address):
-        self.name = name
-        self.birthday = birthday
-        self.address = address
-
-
 class TeachGroup:
     def __init__(self, title, student):
         self.title = title
         self.student = student
+
+
+class Student(TeachGroup):
+
+    def __init__(self, name, birthday, address):
+        self.name = name
+        self.birthday = birthday
+        self.address = address
