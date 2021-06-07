fbee7fee773887b8f24a5ed352931faeac4d8ddf
ObukhovVladislav <vobukhov10@gmail.com>
2021-06-07 12:00:40 +0500

    index.py

--- index.py
+++ index.py
@@ -11,3 +11,12 @@ class Student:
         self.address = address
 
 
+student_1 = Student('Иванов Иван Иванович', '10.02.2002', 'г.Курган. центр')
+student_2 = Student('Сергеев Сергей Сергеевич', '10.02.2002', 'г.Курган. центр')
+group_1 = TeachGroup('ИСИП-33')
+
+group_1.__add__(student_1)
+group_1.__add__(student_2)
+# print()
+group_1.__del__(student_1)
+group_1.__del__(student_2)
